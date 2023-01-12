e956463a82d78468d29f8c00bff92156cbc4740d
AidaGube <aidagube@gmail.com>
2023-01-12 19:55:23 +0600

    last commit

--- JS2/HW-JS2-1/app.js
+++ JS2/HW-JS2-1/app.js
@@ -1,35 +1,46 @@
 // first hw
-// const inputPhone = document.querySelector('#phoneInput')
-// const checkPhone = document.querySelector('#phoneCheck')
-// const resultPhone = document.querySelector('.phoneResult')
+const inputPhone = document.querySelector('#phoneInput')
+const checkPhone = document.querySelector('#phoneCheck')
+const resultPhone = document.querySelector('.phoneResult')
 
-// const phoneRegExp = /^\+7 \d{3} \d{3}-\d{2}-\d{2}$/
+const phoneRegExp = /^\+7 \d{3} \d{3}-\d{2}-\d{2}$/
 
-// checkPhone.addEventListener('click', () => {
-//     console.log(inputPhone.value.match(phoneRegExp));
-//     if (phoneRegExp.test(inputPhone.value)) {
-//         resultPhone.innerText = 'Your number is true'
-//         resultPhone.style.color = 'green'
-//     } else {
-//         resultPhone.innerText = 'Your number is false'
-//         resultPhone.style.color = 'red'
-//     }
-// })
-
-
-//secon hw
-const block2 = document.querySelector('.block2');
-
-block2.addEventListener('click', function myMove() {
-    let position = 0;
-    let id = setInterval(repeat, 25);
-    function repeat() {
-        if (position >= 450) {
-            clearInterval(id);
-        } else {
-            position++;
-            block2.style.left = position + 'px';
-            block2.style.right = position + 'px';
-        }
+checkPhone.addEventListener('click', () => {
+    if (phoneRegExp.test(inputPhone.value)) {
+        resultPhone.innerText = 'Your number is true'
+        resultPhone.style.color = '#19a68c'
+    } else {
+        resultPhone.innerText = 'Your number is false'
+        resultPhone.style.color = 'red'
     }
+    inputPhone.value = ''
 })
+
+
+//second hw
+// function move() {
+//     let position = 0
+//     let timerId = setInterval(time, 20);
+//     function time() {
+//         if (position >= 450) {
+//             clearInterval(timerId)
+//         } else {
+//             position++
+//             document.querySelector('.block2').style.left = position + 'px'
+//         }
+//     }
+// }
+// document.querySelector('.block2').onclick = move
+
+
+//второй вариант 2го задания
+// let position = 0
+// function move() {
+//     position++
+//     document.querySelector('.block2').style.left = position + 'px'
+//     if (position >= 450) {
+//         return true
+//     }
+//     setTimeout(move, 20)
+// }
+// document.querySelector('.block2').onclick = move
