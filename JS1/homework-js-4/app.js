3811283885e122fa74408fdd463456cc2b15fd26
AidaGube <aidagube@gmail.com>
2023-01-17 12:53:39 +0600

    last commit

--- JS1/homework-js-4/app.js
+++ JS1/homework-js-4/app.js
@@ -1,24 +1,37 @@
-var arrays = ['li', 'span', 'div', 'span', 'h3', 'a', 'div', 'span', 'a', 'li', 'div', 'div']; // исходный массив
+var tags = ['li', 'span', 'div', 'span', 'h3', 'a', 'div', 'span', 'a', 'li', 'div', 'div']; // исходный массив
 
-var arr2 = {};
-var arr3 = {};
+var count = {};
 
-for (var array of arrays) {
-  arr2[array] = arr2[array] ? arr2[array] + 1 : 1;
+// tags.forEach(function(a){
+//   if(count[a] !== undefined) {
+//     count[a]++
+//   } else {
+//     count[a] =1
+//   }
+// })
+// // var arr3 = {};
+
+for (var tag of tags) {
+  count[tag] = count[tag] ? count[tag] + 1 : 1;
 }
-console.log(arr2)
+console.log(count)
 
 
-var sortArray = [];
-for (var arr in arr2)
-  sortArray.push([arr, arr2[arr]])
+// // var sorttag = [];
+// // for (var arr in count)
+// //   sorttag.push([arr, count[arr]])
 
-sortArray.sort(
-  function (a, b) {
-    return b[1] - a[1]
-  })
- 
-for (var i = 0; i < sortArray.length; i++) {
-  arr3[sortArray[i][0]] = sortArray[i][1];
-}
-console.log(arr3);
+// // sorttag.sort(
+// //   function (a, b) {
+// //     return b[1] - a[1]
+// //   })
+
+// // for (var i = 0; i < sorttag.length; i++) {
+// //   arr3[sorttag[i][0]] = sorttag[i][1];
+// // }
+// // console.log(arr3);
+
+// console.log(count);
+
+console.log(Object.keys(count).sort((a, b) => count[b] - count[a]
+))
--- JS2/HW-JS3-2/app.js
+++ JS2/HW-JS3-2/app.js
@@ -1,8 +1,11 @@
 /**1)  Доработать прошлое ДЗ! Нужно чтобы малый квадрат теперь двигался по всему родительскому квадрату.
 2) Отобразить на странице цифру 0 , создать 2 кнопки: stop и start
-Если нажать на start то цифра начнет увеличиваться на единицу до того момента пока вы не нажмете на кнопку stop
+Если нажать на start то цифра начнет увеличиваться 
+на единицу до того момента пока вы не нажмете на кнопку stop
 Используйте методы которые мы прошли на уроке
  */
+
+// first hw
 const box = document.querySelector('.box')
 let positionX = 0
 let positionY = 0
@@ -27,3 +30,23 @@ const move = () => {
     }
 }
 move()
+
+
+//second hw
+// const number = document.querySelector('.number')
+// const startBtn = document.querySelector('.start')
+// const stopBtn = document.querySelector('.stop')
+// let timerId;
+
+// startBtn.addEventListener('click', function () {
+//     number.innerHTML
+//     timerId = setInterval(function () {
+//         number.innerHTML++
+//     }, 1000)
+// });
+
+// stopBtn.addEventListener('click', function () {
+//     clearInterval(timerId)
+
+//     // number.innerHTML = 0    //если захотим обнулить
+// });
