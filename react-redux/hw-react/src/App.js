e65def4a0d3ce14b16fc002e39c06a0395c725dc
AidaGube <aidagube@gmail.com>
2023-03-27 21:15:56 +0600

    first

--- react-redux/hw-react/src/App.js
+++ react-redux/hw-react/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Layout from './components/Layout/Layout';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    
+  },[])
   return (
     <div className="App">
       <div className='page__wrapper'>
