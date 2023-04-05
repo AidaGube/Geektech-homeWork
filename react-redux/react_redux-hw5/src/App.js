5498f5f70912d0dafab1114abec6a7ef1223a5e1
AidaGube <aidagube@gmail.com>
2023-04-05 13:06:45 +0600

    hw-5

---
+++ react-redux/react_redux-hw5/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPages from './page/MainPages/MainPages';
+
+function App() {
+  return (
+    <div className="App">
+        <MainPages/>
+    </div>
+  );
+}
+
+export default App;
